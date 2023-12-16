3c3e733d829f92d075b21f7162ea890891549e56
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-12-16 13:49:15 +0100

    Update MinimapPatch.cs

--- Patches/MinimapPatch.cs
+++ Patches/MinimapPatch.cs
@@ -6,24 +6,25 @@ using AutoMapPins.Model;
 using HarmonyLib;
 using JetBrains.Annotations;
 
-namespace AutoMapPins.Patches
+namespace AutoMapPins.Patches;
+
+[HarmonyPatch(typeof(Minimap))]
+class MinimapLoadMapDataPatch
 {
-    [HarmonyPatch(typeof(Minimap), nameof(Minimap.LoadMapData))]
-    class MinimapPatch
+    [UsedImplicitly]
+    // ReSharper disable once InconsistentNaming
+    [HarmonyPatch(nameof(Minimap.LoadMapData))]
+    [HarmonyPostfix]
+    private static void LoadMapDataPostfix(ref Minimap __instance)
     {
-        [UsedImplicitly]
-        // ReSharper disable once InconsistentNaming
-        private static void Postfix(ref Minimap __instance)
+        List<Minimap.PinData> pins = __instance.m_pins;
+        AutoMapPinsPlugin.Log.LogInfo($"Loaded map with {pins.Count} existing pins");
+        foreach (var pin in pins)
         {
-            List<Minimap.PinData> pins = __instance.m_pins;
-            AutoMapPinsPlugin.Log.LogInfo($"Loaded map with {pins.Count} existing pins");
-            foreach (var pin in pins)
-            {
-                PinConfig? config = Registry.ConfiguredPins
-                    .Select(config => config.Value)
-                    .FirstOrDefault(config => config.Name == pin.m_name);
-                if (config != null) pin.m_icon = Assets.GetIcon(config.IconName);
-            }
+            PinConfig? config = Registry.ConfiguredPins
+                .Select(config => config.Value)
+                .FirstOrDefault(config => config.Name == pin.m_name);
+            if (config != null) pin.m_icon = Assets.GetIcon(config.IconName);
         }
     }
 }
